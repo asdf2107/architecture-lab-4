071d4710569fb635a5747612e088ea22ac30dae1
Illia Aprol <asdf21072107@gmail.com>
2021-12-30 18:56:45 +0300

    Use handler in CatCommand

--- src/implementation/catCommand.go
+++ src/implementation/catCommand.go
@@ -1,8 +1,6 @@
 package implementation
 
 import (
-	"fmt"
-
 	"github.com/asdf2107/architecture-lab-4/src/interfaces"
 )
 
@@ -12,5 +10,6 @@ type CatCommand struct {
 }
 
 func (catCommand *CatCommand) Execute(handler interfaces.Handler) {
-	fmt.Println(catCommand.Arg1 + catCommand.Arg2)
+	res := catCommand.Arg1 + catCommand.Arg2
+	handler.Post(&PrintCommand{Arg: res})
 }
